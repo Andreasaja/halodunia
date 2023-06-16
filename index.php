36857c29ef952f0a5f853b5d4b7bfcc431555908
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-16 11:06:18 +0700

    ubah text

--- index.php
+++ index.php
@@ -5,15 +5,8 @@
 <title>Form Login</title>
 </head>
 <body class="text-center"><br><br>
-    <center><h1 class="h3 mb-3 font-weight-normal">WEBBASE COST CONTROL</h1><br>Last Update : 01082021</center><br><br>
-    <div class="login-card">
-        <h1>FORM LOGIN</h1><br>
-        <form method="post" action="supports/validitas.php">
-	        <input type="text" name="username" placeholder="Username">
-	        <input type="password" name="password" placeholder="Password">
-	        <input type="submit" name="login" class="login login-submit" value="login">
-        </form>
-    </div>
-    <center><h4>BEST VIEW : Google Chrome at 1366x768 screen resolution.</h3></center>
+    <center><h1 class="h3 mb-3 font-weight-normal">coba versioning</h1>
+    <br>Last Update : 01082021</center><br><br>
+   
 </body>
 </html> 
