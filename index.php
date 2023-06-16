1fbef1c19f06d5ea61aeeb263134209775aa4cc8
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-16 10:56:07 +0700

    ganti index

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Example</title>
-    </head>
-    <body>
-        <p>lagiii This is an example of a simple HTML page with one paragraph.</p>
-    </body>
-</html>
+<html lang="en">
+<head>
+<link href="desain/index.css" rel="stylesheet" type="text/css"/>
+<title>Form Login</title>
+</head>
+<body class="text-center"><br><br>
+    <center><h1 class="h3 mb-3 font-weight-normal">WEBBASE COST CONTROL</h1><br>Last Update : 01082021</center><br><br>
+    <div class="login-card">
+        <h1>FORM LOGIN</h1><br>
+        <form method="post" action="supports/validitas.php">
+	        <input type="text" name="username" placeholder="Username">
+	        <input type="password" name="password" placeholder="Password">
+	        <input type="submit" name="login" class="login login-submit" value="login">
+        </form>
+    </div>
+    <center><h4>BEST VIEW : Google Chrome at 1366x768 screen resolution.</h3></center>
+</body>
+</html> 
