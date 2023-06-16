4bc3f97ab18c83a2efc6918dec768743757b3464
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-16 11:19:55 +0700

    komit ulang

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <title>Form Login</title>
 </head>
 <body class="text-center"><br><br>
-    <center><h1 class="h3 mb-3 font-weight-normal">coba otomatis versioning</h1>
+    <center><h1 class="h3 mb-3 font-weight-normal">setelah upload cpanel YML</h1>
     <br>Last Update : 01082021</center><br><br>
    
 </body>
