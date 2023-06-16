d9895571fb3200cf3f94318ab05fbedd49360e09
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-16 11:12:28 +0700

    coba otomatis versioning

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <title>Form Login</title>
 </head>
 <body class="text-center"><br><br>
-    <center><h1 class="h3 mb-3 font-weight-normal">coba versioning</h1>
+    <center><h1 class="h3 mb-3 font-weight-normal">coba otomatis versioning</h1>
     <br>Last Update : 01082021</center><br><br>
    
 </body>
