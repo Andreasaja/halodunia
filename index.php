727d70f59fea19a8709e1ac73c92288f58a5f956
Andreasaja <107447911+Andreasaja@users.noreply.github.com>
2023-06-16 12:23:40 +0700

    mbboh

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <title>Form Login</title>
 </head>
 <body class="text-center"><br><br>
-    <center><h1 class="h3 mb-3 font-weight-normal">param folder d hps</h1>
+    <center><h1 class="h3 mb-3 font-weight-normal">param folder d hps dua</h1>
     <br>Last Update : 01082021</center><br><br>
    
 </body>
